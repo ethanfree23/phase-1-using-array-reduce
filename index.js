c52bf86c1aa15c5a1465ffa8fa58267affe83a47
Ethan Freeman <ethanfree23@gmail.com>
2021-11-30 21:18:05 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// let totalBatteries = batteryBatches.reduce(function(totalBatteries, currentValue) {
+//     return totalBatteries + currentValue
+// })
+
+let totalBatteries = batteryBatches.reduce(function(totalBatteries, currentValue){
+    return totalBatteries + currentValue;
+})
+
+// function getTotal(batteryBatches){
+//     let totalBatteries = 0;
+//     for (const batch of batteryBatches) {
+//         totalBatteries += batteryBatches.num;
+//     };
+//     return totalBatteries;
+// }
