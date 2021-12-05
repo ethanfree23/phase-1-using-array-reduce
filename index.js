367c0c24dca8d3f0e2a85e2508df428aff819660
Ethan Freeman <ethanfree23@gmail.com>
2021-12-04 23:27:48 -0600

    commit tree 14feb5bc938a8fd0858c2ce92f39cde72ab3c27f

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 //     return totalBatteries + currentValue
 // })
 
-let totalBatteries = batteryBatches.reduce(function(totalBatteries, currentValue){
-    return totalBatteries + currentValue;
+// let totalBatteries = batteryBatches.reduce(function(totalBatteries, currentValue){
+//     return totalBatteries + currentValue;
+// })
+
+let totalBatteries = batteryBatches.reduce(function (totalBatteries, currentValue) {
+    return totalBatteries+currentValue
 })
 
 // function getTotal(batteryBatches){
@@ -17,3 +21,32 @@ let totalBatteries = batteryBatches.reduce(function(totalBatteries, currentValue
 //     };
 //     return totalBatteries;
 // }
+
+
+/*
+NOTES:
+
+Given a list of shopping items, we want to find the total price:
+
+const products = [
+  { name: 'Shampoo', price: 4.99 },
+  { name: 'Donuts', price: 7.99 },
+  { name: 'Cookies', price: 6.49 },
+  { name: 'Bath Gel', price: 13.99 }
+];
+
+function getTotalAmountForProducts(products) {
+  let totalPrice = 0;
+
+  for (const product of products) {
+    totalPrice += product.price;
+  };
+
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products)); // LOG: 33.46
+
+we make a function(products), with a starting totalPrice of $0, then, for(let product of prodcts) we return the totalPrice +=product.price to find the
+
+ */
